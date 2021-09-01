a4c68c106534f9925087e360d3351042625299a1
PRO-Coding <80043809+pro-whitehatjr@users.noreply.github.com>
2021-09-02 00:53:57 +0530

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -11,8 +11,13 @@ class CannonBall {
   }
 
   shoot() {
+    var newAngle = cannon.angle - 28;
+    newAngle = newAngle *(3.14/180)
+    var velocity = p5.Vector.fromAngle(newAngle);
+    velocity.mult(0.5);
     Matter.Body.setStatic(this.body, false);
-    Matter.Body.setVelocity(this.body, { x: 30, y: -20 });
+    Matter.Body.setVelocity(this.body, {
+      x: velocity.x *(180/3.14), y: velocity.y * (180/3.14)});
   }
 
   display() 
