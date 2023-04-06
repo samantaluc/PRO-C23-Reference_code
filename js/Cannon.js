4950086f0885edc8967c4937add7262d1e810751
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-04-06 14:08:20 -0300

    Update Cannon.js

--- js/Cannon.js
+++ js/Cannon.js
@@ -4,24 +4,23 @@ class Cannon {
     this.y = y;
     this.width = width;
     this.height = height;
-    this.angle = angle;
+    this.angle = angle; // esse angulo
     this.cannon_image = loadImage("assets/canon.png");
     this.cannon_base = loadImage("assets/cannonBase.png");
   }
   
   display() {
-    console.log(this.angle)
-    if (keyIsDown(RIGHT_ARROW) && this.angle<70  ) {
-      this.angle += 1;
+    if (keyIsDown(RIGHT_ARROW) && this.angle<70  ) { //se a tecla abaixada é a seta direita E o angulo é menor que 70 graus
+      this.angle += 1; //aumente o angulo atual em +1
     }
 
-    if (keyIsDown(LEFT_ARROW) && this.angle>-30 ) {
-      this.angle -= 1;
+    if (keyIsDown(LEFT_ARROW) && this.angle>-30 ) { //se a tecla abaixada é a seta esquerda E o angulo é menor que -30 graus(210 da direita para esquerda) 
+      this.angle -= 1; //diminua o angulo atual em -1
     }
 
-    push();
-    translate(this.x, this.y);
-    rotate(this.angle);
+    push(); 
+    translate(this.x, this.y); //traduz/modifica o ponto para rotação da ponta para o centro da imagem
+    rotate(this.angle); //rotaciona de acordo com o valor do angulo
     imageMode(CENTER);
     image(this.cannon_image,0,0, this.width, this.height);
     pop();
